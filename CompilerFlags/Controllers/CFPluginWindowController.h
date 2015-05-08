fe03e7ff4cc164b489fd530fd7ba08492cb4b9dc
yuxiaopeng <yuxiaopeng@breadtrip.com>
2015-05-08 18:52:50 +0800

    first commit

---
+++ CompilerFlags/Controllers/CFPluginWindowController.h
@@ -0,0 +1,13 @@
+//
+//  CFPluginWindowController.h
+//  CompilerFlags
+//
+//  Created by 于晓鹏 on 15/5/8.
+//  Copyright (c) 2015年 roc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CFPluginWindowController : NSWindowController
+
+@end
---
+++ CompilerFlags/Controllers/CFPluginWindowController.m
@@ -0,0 +1,23 @@
+//
+//  CFPluginWindowController.m
+//  CompilerFlags
+//
+//  Created by 于晓鹏 on 15/5/8.
+//  Copyright (c) 2015年 roc. All rights reserved.
+//
+
+#import "CFPluginWindowController.h"
+
+@interface CFPluginWindowController ()
+
+@end
+
+@implementation CFPluginWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
